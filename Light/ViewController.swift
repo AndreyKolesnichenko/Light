fd32f71fe6a7d7c2d44d9a750264afff32dd91bb
Андрей Колесниченко <Freestar.ua@gmail.com>
2018-11-05 15:18:59 +0200

    рабочий

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -5,11 +5,39 @@
 //  Created by Andrey on 05.11.2018.
 //  Copyright © 2018 Andrey. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightSend = true
+    
+    @IBAction func LightBatton(_ sender: UIButton) {
+        lightSend = !lightSend
+        updeteWiew()
+    }
+    
+    func updeteWiew()  {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        if let dev = device, dev.hasTorch {
+            do {
+             try dev.lockForConfiguration()
+                dev.torchMode = lightSend ? .on : .off
+            } catch {
+                print("Error")
+            }
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
